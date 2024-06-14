bfa91e2f361a5915247100b5f3a46d34cd40dc74
mfq17s <hanifabubakari12345@gmail.com>
2024-06-14 14:44:34 +0000

    fire

---
+++ firebase/firebase.jsx
@@ -0,0 +1,15 @@
+
+import { firebase } from "firebase/app";
+import "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyBd2l7jCJD1t7ciV9-2wPxnqvJJJ-uANz0",
+  authDomain: "result-checker-5b29d.firebaseapp.com",
+  projectId: "result-checker-5b29d",
+  storageBucket: "result-checker-5b29d.appspot.com",
+  messagingSenderId: "352242339768",
+  appId: "1:352242339768:web:256dc803491a66f110894d",
+  measurementId: "G-K7N5L51WFZ"
+};
+
+
+firebase.initializeApp(firebaseConfig);
